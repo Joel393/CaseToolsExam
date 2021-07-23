165dbf1207c6c33070abd6333a9901c44fceaa23
Joel <Joel@LAPTOP-0CCLVLEH>
2021-07-23 14:22:16 +0530

    First Commit

---
+++ CaseToolsExam/src/Fruit_120.java
@@ -0,0 +1,15 @@
+
+public class Fruit_120 {
+	
+	String color;
+	Fruit_120(String color)
+	{
+	this.color=color;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
